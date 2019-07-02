4609e85be3f2beab5789ab39e9ed63615194e45c
Mithra Singam <mithra.singam@move.com>
2019-07-02 12:05:27 -0700

    Created template for Network Manager

---
+++ SFFoodTrucks/Network Manager/GetFoodTrucksList.swift
@@ -0,0 +1,13 @@
+//
+//  GetFoodTrucksList.swift
+//  SFFoodTrucks
+//
+//  Created by Singam, Venkata Srinath Mith on 7/2/19.
+//  Copyright © 2019 Mithra Singam. All rights reserved.
+//
+
+import UIKit
+
+class GetFoodTrucksList: NSObject {
+
+}
